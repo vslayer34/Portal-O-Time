630ce2d0630c16a775545433b6ade59d0014c6f0
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-07-28 11:37:38 +0300

    Added blue and orange portals prefab

--- Assets/Scripts/Portals/Portal.cs
+++ Assets/Scripts/Portals/Portal.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Portal : MonoBehaviour
+public abstract class Portal : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
