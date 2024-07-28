80b4275bcf242e44f2d0b2a283b56bd004630c35
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-07-28 12:46:08 +0300

    Added the basic logic for shooting portal projectiles

--- Assets/Scripts/Guns/PortalGun.cs
+++ Assets/Scripts/Guns/PortalGun.cs
@@ -1,8 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PortalGun : MonoBehaviour, IWeapon
 {
-    
+    [SerializeField, Tooltip("Reference to the player shooting script")]
+    private ShootPortals _playerPortalControls;
+
+    [SerializeField, Tooltip("Portal projectile")]
+
+    private PortalType _portalType;
+
+
+
+    // Game Loop methods---------------------------------------------------------------------------
+    private void Start()
+    {
+        _playerPortalControls.OnPortalGunFired += ShootPortals_OnPortalGunFired;
+    }
+
+    // Member Methods------------------------------------------------------------------------------
+    private void ShootPortal(PortalType portalType)
+    {
+
+    }
+    // Signal Methods------------------------------------------------------------------------------
+
+    private void ShootPortals_OnPortalGunFired(object sender, ShootPortals.OnPortalGunFiredEventArgs e)
+    {
+        
+    }
 }
--- Assets/Scripts/Player/ShootPortals.cs
+++ Assets/Scripts/Player/ShootPortals.cs
@@ -1,8 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+
+public enum PortalType
+{
+    Blue,
+    Orange
+}
 
 public class ShootPortals : MonoBehaviour
 {
-    
+    public event EventHandler<OnPortalGunFiredEventArgs> OnPortalGunFired;
+    public class OnPortalGunFiredEventArgs : EventArgs
+    {
+        public PortalType portalType;
+    }
+
+
+
+    // Game Loop Methods---------------------------------------------------------------------------
+    private void Start()
+    {
+        PlayerInput.Instance.OnPrimaryFire += PlayerInput_PrimaryFire;
+        PlayerInput.Instance.OnSecondaryFire += PlayerInput_SecondaryFire;
+    }
+
+    private void OnDisable()
+    {
+        PlayerInput.Instance.OnPrimaryFire -= PlayerInput_PrimaryFire;
+        PlayerInput.Instance.OnSecondaryFire -= PlayerInput_SecondaryFire;
+    }
+    // Member Methods------------------------------------------------------------------------------
+    // Signal Methods------------------------------------------------------------------------------
+    private void PlayerInput_PrimaryFire()
+    {
+        Debug.Log("Primary fire");
+        OnPortalGunFired?.Invoke(this, new OnPortalGunFiredEventArgs
+        {
+            portalType = PortalType.Blue
+        });
+    }
+
+    private void PlayerInput_SecondaryFire()
+    {
+        Debug.Log("Secondary fire");
+
+        OnPortalGunFired?.Invoke(this, new OnPortalGunFiredEventArgs
+        {
+            portalType = PortalType.Orange
+        });
+        
+    }
 }
